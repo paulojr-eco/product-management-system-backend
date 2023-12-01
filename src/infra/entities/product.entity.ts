1dcfc1875a8cd1c33e2f3ade1b9c9598e34fc694
Paulo Paiva <paulojr.eco@gmail.com>
2023-12-01 17:33:07 -0300

    feat: add product entity

---
+++ src/infra/entities/product.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'products' })
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    length: 60,
+  })
+  descricao: string;
+
+  @Column({ type: 'decimal', scale: 1, nullable: true })
+  custo: number;
+
+  @Column({ nullable: true })
+  imagem: string;
+}
