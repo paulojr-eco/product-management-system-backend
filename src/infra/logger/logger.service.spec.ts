91649c0ca427d21f104b67a2e79a8083b571a435
Paulo Paiva <paulojr.eco@gmail.com>
2023-12-01 18:48:30 -0300

    test: ensure logger methods are called with correct values

---
+++ src/infra/logger/logger.service.spec.ts
@@ -0,0 +1,47 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { LoggerService } from './logger.service';
+
+type GenericError = {
+  context: string;
+  message: string;
+  trace?: string;
+};
+
+const mockError = (): GenericError => ({
+  context: 'any context',
+  message: 'any message',
+  trace: 'any trace',
+});
+
+describe('LoggerService', () => {
+  let service: LoggerService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [LoggerService],
+    }).compile();
+
+    service = module.get<LoggerService>(LoggerService);
+  });
+
+  it('should call debug with correct values', () => {
+    const { context, message } = mockError();
+    const debugSpy = jest.spyOn(service, 'debug');
+    service.debug(context, message);
+    expect(debugSpy).toHaveBeenCalledWith(context, message);
+  });
+
+  it('should call log with correct values', () => {
+    const { context, message } = mockError();
+    const logSpy = jest.spyOn(service, 'log');
+    service.log(context, message);
+    expect(logSpy).toHaveBeenCalledWith(context, message);
+  });
+
+  it('should call error with correct values', () => {
+    const { context, message, trace } = mockError();
+    const errorSpy = jest.spyOn(service, 'error');
+    service.error(context, message, trace);
+    expect(errorSpy).toHaveBeenCalledWith(context, message, trace);
+  });
+});
