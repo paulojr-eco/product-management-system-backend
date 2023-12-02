2011dcb4be8b699a83000cfc27b2962b3e09415a
Paulo Paiva <paulojr.eco@gmail.com>
2023-12-02 22:40:15 +0000

    feat: compose app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ExceptionsModule } from './infra/exceptions/exceptions.module';
+import { RepositoriesModule } from './infra/repositories/repositories.module';
+import { LoggerModule } from './infra/logger/logger.module';
+import { UsecasesProxyModule } from './infra/usecases-proxy/usecases-proxy.module';
+import { EnvironmentConfigModule } from './main/config/environment-config/environment-config.module';
+import { ControllersModule } from './infra/controllers/controllers.module';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [
+    ExceptionsModule,
+    RepositoriesModule,
+    LoggerModule,
+    UsecasesProxyModule.register(),
+    EnvironmentConfigModule,
+    ControllersModule,
+  ],
   providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionFilter } from './infra/common/filters/exception.filter';
+import { LoggerService } from './infra/logger/logger.service';
+import { ResponseInterceptor } from './infra/common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new AllExceptionFilter(new LoggerService()));
+
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  app.setGlobalPrefix('api');
+
   await app.listen(3000);
 }
 bootstrap();
