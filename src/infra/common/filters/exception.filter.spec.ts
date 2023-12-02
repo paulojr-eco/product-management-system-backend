853d86200e4a3944472ce9b9cf0b48ee6ee713cd
Paulo Paiva <paulojr.eco@gmail.com>
2023-12-02 17:28:28 +0000

    test: ensure exception filter is catching generic and http exceptions

---
+++ src/infra/common/filters/exception.filter.spec.ts
@@ -0,0 +1,67 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AllExceptionFilter } from './exception.filter';
+import { LoggerService } from '../../logger/logger.service';
+import { ArgumentsHost, HttpStatus } from '@nestjs/common';
+import { HttpException } from '@nestjs/common/exceptions';
+
+const mockGenericException = () => new Error('any message');
+const mockHttpException = () =>
+  new HttpException('another message', HttpStatus.BAD_REQUEST);
+
+const mockHost = () =>
+  ({
+    switchToHttp: () => ({
+      getResponse: () => ({
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      }),
+      getRequest: () => ({
+        method: 'GET',
+        url: '/test',
+        path: '/test',
+      }),
+    }),
+  }) as ArgumentsHost;
+
+describe('AllExceptionFilter', () => {
+  let filter: AllExceptionFilter;
+  let loggerService: LoggerService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [AllExceptionFilter, LoggerService],
+    }).compile();
+
+    filter = module.get<AllExceptionFilter>(AllExceptionFilter);
+    loggerService = module.get<LoggerService>(LoggerService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should catch and handle generic exception', () => {
+    const exception = mockGenericException();
+    const host = mockHost();
+    const errorSpy = jest.spyOn(loggerService, 'error');
+    const request = host.switchToHttp().getRequest();
+    filter.catch(exception, host);
+    expect(errorSpy).toHaveBeenCalledWith(
+      `End Request for ${request.path}`,
+      `method=${request.method} status=500 message=any message`,
+      exception.stack,
+    );
+  });
+
+  it('should catch and handle http exception', () => {
+    const exception = mockHttpException();
+    const host = mockHost();
+    const warnSpy = jest.spyOn(loggerService, 'warn');
+    const request = host.switchToHttp().getRequest();
+    filter.catch(exception, host);
+    expect(warnSpy).toHaveBeenCalledWith(
+      `End Request for ${request.path}`,
+      `method=${request.method} status=${HttpStatus.BAD_REQUEST} message=another message`,
+    );
+  });
+});
