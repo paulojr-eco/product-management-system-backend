5cb36fb181c296b9ece7c4106ae2895961af66a9
Paulo Paiva <paulojr.eco@gmail.com>
2023-12-02 17:30:10 +0000

    test: ensure responses are being intercepted and have the correct props

---
+++ src/infra/common/interceptors/response.interceptor.spec.ts
@@ -0,0 +1,50 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { ResponseInterceptor, ResponseFormat } from './response.interceptor';
+import { Observable, of } from 'rxjs';
+
+class MockData {
+  prop: string;
+}
+
+@Controller()
+class TestController {
+  @Get('test')
+  @UseInterceptors(ResponseInterceptor)
+  test(): Observable<MockData> {
+    const mockData: MockData = {
+      prop: 'data',
+    };
+    return of(mockData);
+  }
+}
+
+describe('ResponseInterceptor', () => {
+  let app;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [TestController],
+      providers: [ResponseInterceptor],
+    }).compile();
+
+    app = module.createNestApplication();
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('should intercept and format the response', async () => {
+    const response = await request(app.getHttpServer()).get('/test');
+    const responseBody: ResponseFormat<MockData> = response.body;
+    expect(response.status).toBe(200);
+    expect(responseBody.isArray).toBe(false);
+    expect(responseBody.path).toBe('/test');
+    expect(responseBody.duration).toMatch(/^\d+ms$/);
+    expect(responseBody.method).toBe('GET');
+    expect(responseBody.data).toEqual({ prop: 'data' });
+  });
+});
