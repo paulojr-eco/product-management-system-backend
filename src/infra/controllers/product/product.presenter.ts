19ce0eab421d4c536a031c6f4bc94fe4ae574b90
Paulo Paiva <paulojr.eco@gmail.com>
2023-12-02 20:05:20 +0000

    feat: add product presenter response

---
+++ src/infra/controllers/product/product.presenter.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Product } from '../../../domain/models/product';
+
+export class ProductPresenter {
+  @ApiProperty()
+  id: number;
+  @ApiProperty()
+  description: string;
+  @ApiProperty()
+  cost: number;
+  @ApiProperty()
+  image: string;
+
+  constructor(product: Product) {
+    this.id = product.id;
+    this.description = product.description;
+    this.cost = product.cost;
+    this.image = product.image;
+  }
+}
