977b6f99973a6240f924dc4f739ac28b4c1f6679
Paulo Paiva <paulojr.eco@gmail.com>
2023-12-04 12:45:40 -0300

    refactor: fix typo

--- src/domain/usecases/product/add-product.ts
+++ src/domain/usecases/product/add-product.ts
@@ -2,7 +2,7 @@ import { ProductStore } from '../../../domain/models/product-store';
 import { Product } from '../../../domain/models/product';
 
 export type AddProductParams = Omit<Product, 'id'>;
-export type AddProductStoreParams = Omit<ProductStore, 'id' | 'idProduct'>;
+export type AddProductStoreParams = Omit<ProductStore, 'id' | 'idProduto'>;
 
 export interface IAddProduct {
   add: (
