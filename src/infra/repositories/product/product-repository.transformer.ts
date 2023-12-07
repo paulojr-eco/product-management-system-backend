b8bffcc73527ce2de9043b840dcd5ad8ca376279
Paulo Paiva <paulojr.eco@gmail.com>
2023-12-07 13:25:02 -0300

    refactor: remove unecessary code

--- src/infra/repositories/product/product-repository.transformer.ts
+++ src/infra/repositories/product/product-repository.transformer.ts
@@ -1,5 +1,4 @@
 import { AddProductParams } from 'src/domain/usecases/product/add-product';
-import { Product as ProductModel } from '../../../domain/models/product';
 import { Product as ProductEntity } from '../../../infra/entities/product.entity';
 
 export function toProductEntity(product: AddProductParams): ProductEntity {
@@ -8,14 +7,4 @@ export function toProductEntity(product: AddProductParams): ProductEntity {
   productEntity.custo = product.custo;
   productEntity.imagem = product.imagem;
   return productEntity;
-}
-
-export function toProductModel(productEntity: ProductEntity): ProductModel {
-  const product: ProductModel = {
-    id: productEntity.id,
-    descricao: productEntity.descricao,
-    custo: productEntity.custo,
-    imagem: productEntity.imagem,
-  };
-  return product;
 }
